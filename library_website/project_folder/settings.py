309ad66f3ae9cd8d93567300ae10a237057e65af
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-09-27 14:10:11 +0000

    css file succesfully  connected to the html

--- library_website/project_folder/settings.py
+++ library_website/project_folder/settings.py
@@ -117,9 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATICFILES_DIRS = [
+    BASE_DIR /
+    "app_folder/static/",
+]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
