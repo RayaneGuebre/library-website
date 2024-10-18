b118cb7027bcdcc5664a435b86957e98b05466ae
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-10-18 09:07:19 +0000

    still working on categories pages

--- website/app_folder/admin.py
+++ website/app_folder/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
 from .models import Category, Book
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'isbn', 'is_available')
-
-admin.site.register(Book, BookAdmin)
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)  # Display these fields in the list view
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'publication_date', 'available', 'isbn', 'category')
-
+    list_display = ('name', 'author', 'publication_date', 'is_available', 'isbn', 'category', 'is_available')
