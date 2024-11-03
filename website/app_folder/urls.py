383e065a7414b16229c7ad4d3097cc75413a5c3e
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-11-03 16:58:57 +0000

    Successfuly  imported Cover Images using google books API

--- website/app_folder/urls.py
+++ website/app_folder/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.home, name='category_list'),  # Home page showing all categories
     path('<str:category_name>/', views.books_in_category, name='books_in_category'),
     path('<str:category_name>/<str:book_slug>/', views.book_detail, name='book_detail'),  # Page for a specific book
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- website/app_folder/views.py
+++ website/app_folder/views.py
@@ -1,6 +1,9 @@
+import os
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Book, FeaturedBooks
-
+import requests
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def home(request):
@@ -16,9 +19,25 @@ def books_in_category(request, category_name):
     return render(request, 'app_folder/category.html', {'category': category, 'books': books})
 
 def book_detail(request, book_slug, category_name):
+    
     category = get_object_or_404(Category, name=category_name)
     book = get_object_or_404(Book, slug=book_slug)
-    cover_url = f"http://covers.openlibrary.org/b/isbn{book.isbn}-L.jpg"
+    isbn = book.isbn
+    api_key = os.getenv("GOOGLE_BOOKS_API_KEY")
+    cover_url = None
+    url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={api_key}"
+    print(url)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        if "items" in data:
+            cover_url = data["items"][0]["volumeInfo"].get("imageLinks", {}).get("thumbnail")
+            if cover_url:
+                cover_url = cover_url.replace("zoom=1", "zoom=2").replace("zoom=2", "zoom=3")
+    else: 
+        print("Failed to find the cover")
+    
     return render(request, 'app_folder/book_detail.html', {'book': book,'cover_url' : cover_url})
 
 
