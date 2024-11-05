3cc3a2fe11f5e0704decdbb751794300bba2c2cb
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-11-05 10:11:18 +0000

    adding some things

--- website/app_folder/urls.py
+++ website/app_folder/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='category_list'),  # Home page showing all categories
     path('<str:category_name>/', views.books_in_category, name='books_in_category'),
-    path('<str:category_name>/<str:book_slug>/', views.book_detail, name='book_detail'),  # Page for a specific book
+    path('<str:category_name>/<slug:book_slug>/', views.book_detail, name='book_detail')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
